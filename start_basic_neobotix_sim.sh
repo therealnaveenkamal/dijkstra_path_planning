5a10faf77c1641a3491f9b466cd41c9934dee1d9
Roberto Zegers <rzegers@uc.cl>
2023-06-26 09:07:59 +0000

    Remove 'neo_simulation2' package from repository

--- start_basic_neobotix_sim.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash 
-
-NEOBOTIX_SIM_PATH=/home/user/ros2_ws/install/setup.bash
-source $NEOBOTIX_SIM_PATH
-export GAZEBO_MODEL_PATH=/home/user/ros2_ws/src/neobotix_ros2/neo_simulation2/models:/home/user/ros2_ws/src:/home/user/ros2_ws/src/neobotix_ros2
-ros2 launch neo_simulation2 simulation_basics.launch.py
--- start_warehouse_neobotix_sim.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash 
-
-NEOBOTIX_SIM_PATH=/home/simulations/ros2_sims_ws/install/setup.bash
-source $NEOBOTIX_SIM_PATH
-export GAZEBO_MODEL_PATH=/home/simulations/ros2_sims_ws/src/neobotix_ros2/neo_simulation2/models:/home/simulations/ros2_sims_ws/src:/home/simulations/ros2_sims_ws/src/neobotix_ros2:/home/simulations/ros2_sims_ws/src/aws_simulations/aws-robotics/aws-robomaker-small-warehouse-world/models
-ros2 launch neo_simulation2 simulation_warehouse.launch.py
